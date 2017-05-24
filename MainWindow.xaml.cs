9e275ad1af5e3a5e019bd2282a7f645f99736198
Ian Berthon <ian@summerleaze.biz>
2017-05-24 21:33:23 +0100

    Add project Files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+using System.Reflection;
+
+namespace RemindersWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Assembly _assembly;
+
+            try
+            {
+                _assembly = Assembly.GetExecutingAssembly();
+
+                using (StreamReader sr = new StreamReader(_assembly.GetManifestResourceStream("RemindersWPF.onthisday.sql")))
+                {
+                    string s = sr.ReadToEnd();
+                    textBlock.Text = s;
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Error accessing resources!");
+            }
+
+        }
+    }
+}
